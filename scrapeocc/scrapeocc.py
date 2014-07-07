9634c9fe8dc1496a1dfe4eb44f99b8c2aa218da5
Kevin Manley <kevin.manley+github@gmail.com>
2014-07-07 08:28:12 -0400

    generalize and include ridgefield studio

--- scrapeocc/scrapeocc.py
+++ scrapeocc/scrapeocc.py
@@ -8,22 +8,44 @@ import twitter
 from BeautifulSoup import BeautifulSoup as BS
 import datetime
 secrets = open(".secret").read().strip().split(";")
-baseurl, username, password = secrets
+username, password = secrets
 logging.basicConfig()
 log = logging.getLogger()
 log.setLevel(logging.WARNING)
 
 conn = sqlite3.connect("occupancy.db")
-SITENAMES = ["westport", "darien"]
-SITES = {"westport":"http://www.%s/reserve/index.cfm?action=Reserve.chooseClass&site=1&n=Westport" % baseurl,
-	 "darien":"http://www.%s/reserve/index.cfm?action=Reserve.chooseClass&site=3&n=Darien" % baseurl}
-CAPACITY = {"westport":44, "darien":40}
+
+SITENAMES = ["westport", "darien", "ridgefield"] # TODO: texas
+
+BASEURL = {"westport" : "http://www.joyridestudio.com",
+           "darien" : "http://www.joyridestudio.com",
+           "ridgefield" : "http://www.joyrideridgefield.com"}
+
+USERAGENT = {"User-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36"}
+
+LOGINGET = {"westport" : "http://www.joyridestudio.com/reserve/index.cfm?action=Account.login",
+            "darien"   : "http://www.joyridestudio.com/reserve/index.cfm?action=Account.login",
+            "ridgefield" : "http://www.joyrideridgefield.com/reserve/index.cfm?action=Account.login",}
+
+LOGINPOST = {"westport" : "http://www.joyridestudio.com/reserve/index.cfm?action=",
+            "darien"   : "http://www.joyridestudio.com/reserve/index.cfm?action=",
+            "ridgefield" : "http://www.joyrideridgefield.com/reserve/index.cfm?action=",}
+
+CALENDARGET = {"westport":  "http://www.joyridestudio.com/reserve/index.cfm?action=Reserve.chooseClass&site=1&n=Westport",
+	       "darien":    "http://www.joyridestudio.com/reserve/index.cfm?action=Reserve.chooseClass&site=3&n=Darien",
+               "ridgefield":"http://www.joyrideridgefield.com/reserve/index.cfm?action=Reserve.chooseClass"} # TODO:
+
+CAPACITY = {"westport":44, 
+	    "darien":40, 
+            "ridgefield":35} # TODO: 35 or 36?
+
 TODAY = datetime.datetime.today()
+TOMORROW = TODAY + datetime.timedelta(days=1)
 
 # TODO: sold out classes (classfull class) have a link that does not contain date/time info, just a "sorry, class full msg"
 # so we need to extract date/time/instructor here instead of in getOccupancy
 def getBookableLinks(site, cookies):
-	r = requests.get(SITES[site], headers=userAgent, cookies=cookies)
+	r = requests.get(CALENDARGET[site], headers=userAgent, cookies=cookies)
 	soup = BS(r.text)
 	blocks = soup.findAll("div", attrs={"class":["scheduleBlock", "scheduleBlock classfull"]})
 	for block in blocks:
@@ -31,7 +53,6 @@ def getBookableLinks(site, cookies):
 	    link = block.find("a")
 	    if link:
 		if link.span.text and link.span.text.lower().find("cycle")>-1:
-  		    #print link.span.text, link.get("href")
                     day = int(link.parent.parent["class"][3:])
                     sdate = soup.findAll("span", attrs={"class":"thead-date"})[day].text
                     dateparts = sdate.split(".")
@@ -52,10 +73,10 @@ def getBookableLinks(site, cookies):
                     dt = datetime.datetime(year, month, day, hour, minute, 0)
                     yield dt, instr, soldout, link.get("href")
 	    else:
-		pass #print "no link"
+		pass #not bookable - class is in past or cancelled
 
-def getOccupancy(url, cookies):
-    r = requests.get("http://www.%s%s" % (baseurl, url), headers=userAgent, cookies=cookies)
+def getOccupancy(site, url, cookies):
+    r = requests.get("%s%s" % (BASEURL[site], url), headers=userAgent, cookies=cookies)
     soup = BS(r.text)
     details = soup.find("div", attrs={"class":"yui-u", "id":"sidebar"})
     if not details:
@@ -89,25 +110,16 @@ def getOccupancy(url, cookies):
     occ = float(unavail) / total * 100.
     return dt, instr, unavail, total, occ
 
-userAgent = {"User-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36"}
-NDAYS = 1 
-target_date = datetime.date.today() + datetime.timedelta(days=NDAYS)
-starget_date = target_date.strftime("%a %b") + " " + str(target_date.day)
+def loginGetCookies(sitename):
+    r = requests.get(LOGINGET[sitename], headers=USERAGENT, allow_redirects=True)
+    cookies = r.cookies
+    formdata = {'action': 'Account.doLogin', 'username': username, 'password':password}
+    r = requests.post(LOGINPOST[sitename], data=formdata, headers=userAgent, cookies=cookies, allow_redirects=False)
+    return r.cookies
 
-r = requests.get("http://www.%s/reserve/index.cfm?action=Account.login" % baseurl, headers=userAgent, allow_redirects=True)
-cookies = r.cookies
-formdata = {'action': 'Account.doLogin', 'username': username, 'password':password}
-r = requests.post("https://www.%s/reserve/index.cfm?action=" % baseurl, data=formdata, headers=userAgent, cookies=cookies, allow_redirects=False)
-
-# this gets calendar page; need to find td class="... today" to get classids of today's classes
-# 	<div class="scheduleBlock">
-#	<a href="/reserve/index.cfm?action=Reserve.chooseSpot&amp;classid=16601">
-#r = requests.get("http://www.%s/reserve/index.cfm?action=Reserve.chooseClass&site=3&n=Darien" % baseurl, cookies=cookies)
-
-curs = conn.cursor()
-tomorrow = datetime.date.today() + datetime.timedelta(days=1)
-for sitename in SITENAMES:
-    print sitename
+def processSite(sitename, saveToDB=False):
+    tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+    cookies = loginGetCookies(sitename)
     for item in getBookableLinks(sitename, cookies):
         dt, instr, soldout, url = item
         if dt.date() > tomorrow:
@@ -117,10 +129,20 @@ for sitename in SITENAMES:
         else:
             dt, instr, unavail, total, occ = getOccupancy(url, cookies)
         print str(dt), instr, "%d/%d" % (unavail,total), "%.1f%%" % occ
-        curs.execute("insert or replace into occ values (?, ?, ?, ?)", (dt, instr, unavail, total))
-        conn.commit()
+        if saveToDB:
+            curs = conn.cursor()
+            curs.execute("insert or replace into occ values (?, ?, ?, ?)", (dt, instr, unavail, total))
+            conn.commit()
 
-    print "-" * 40
+def main(saveToDB=False):
+    for sitename in SITENAMES:
+        print sitename
+        processSite(sitename, saveToDB)
+        print "-" * 80
 
-
+if __name__ == "__main__":
+    saveToDB = False
+    if "-db" in sys.argv:
+        saveToDB = True
+    main(saveToDB)
 
