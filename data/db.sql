36d3704b997e6772cfa34d5061737591805964b2
Kevin Manley <kevin.manley+github@gmail.com>
2014-07-16 21:55:01 -0400

    added more useful queries

--- data/db.sql
+++ data/db.sql
@@ -52,8 +52,19 @@ id INTEGER PRIMARY KEY AUTOINCREMENT
 create view v_sale as select * from sale left join cust on sale.custid=cust.id;
 
 -- get sales by city
-select lower(city), sum(total) from v_sale group by lower(city) order by sum(total) desc;
+select lower(city), sum(total) from v_sale group by lower(city) having sum(total)>0 order by sum(total) desc;
 
+-- sales by city by month
+select strftime('%Y-%m',dt) as yymm, lower(city) as cty, sum(total) as ttl 
+from v_sale 
+where cty in ('ridgefield','wilton') 
+group by yymm, cty 
+having ttl>0 
+order by yymm,cty;
+
+create view vw_birthday as select firstname, lastname, emailaddress, birthdate, strftime("%m-%d", birthdate) as mmdd from cust;
+
+select * from vw_birthday where mmdd = '07-17';
 
 
 
