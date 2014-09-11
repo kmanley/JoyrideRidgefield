8bc43ccfb4b4bec741015ca7c32a26178bccefa7
Kevin Manley <kevin.manley+github@gmail.com>
2014-09-11 18:29:27 -0400

    track studio total rides

--- data/import_attendance.py
+++ data/import_attendance.py
@@ -9,10 +9,12 @@ def main(csvfile):
 	curs = conn.cursor()
 	curs.execute("delete from attend")
 	enrolled = {} # custid -> enroll count
+	totenrolled = 0 # total enrolled across all customers
     # 0x0d line terminators - I told you zingfit sucks! 
 	lines = [x.strip() for x in open(csvfile, "rb").read().split("\r")]
+	rows = []
+	# first pass, build up rows
 	for i, line in enumerate(lines):
-		if i==0: continue
 		cols = []
 		j = 0
 		inquote = False
@@ -31,17 +33,40 @@ def main(csvfile):
 			return
 			sys.exit(2)
 		cols = [unicode(col[1:-1] if col.startswith('"') else col, encoding="latin-1") for col in cols]
-		custid = cols[1]
-		status = cols[5]
+		#custid = cols[1]
+		#status = cols[5]
+		rows.append(cols)
+
+	header = rows[0]
+	idx_custid = header.index("CUSTOMERID")
+	idx_status = header.index("STATUS")
+	idx_classdate = header.index("CLASSDATE")
+	idx_datein = header.index("DATEIN")
+	#print idx_classdate, idx_datein
+	#import sys
+	#sys.exit(0)
+	# eliminate header
+	rows = rows[1:]
+	rows.sort(lambda lhs, rhs: cmp(lhs[idx_classdate], rhs[idx_classdate]) or cmp(lhs[idx_datein], rhs[idx_datein]))
+	#for row in rows:
+	#	print row[0], row[1], row[2], row[3], row[7], row[10]
+	#import sys
+	#sys.exit(0)
+		
+	for i, row in enumerate(rows):
+		custid = row[idx_custid]
+		status = row[idx_status]
+		print custid, status
 		enrolled.setdefault(custid, 0)
 		if status == "Enrolled":
 			enrolled[custid] = enrolled[custid] + 1
+			totenrolled += 1
 			
 		#print cols
 		if i>0 and i % 1000 == 0:
 			print "%d rows..." % i
-		curs.execute("insert into attend values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
-	             tuple(cols)+(enrolled[custid],))
+		curs.execute("insert into attend values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+	             tuple(row)+(enrolled[custid],totenrolled))
 	conn.commit()
 	print "loaded %d attendance rows" % i
 
