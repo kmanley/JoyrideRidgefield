99228477963e6def07cc25c94c1b6cca89e3afc2
Kevin Manley <kevin.manley+github@gmail.com>
2014-09-15 08:40:37 -0400

    temp hack to show fashion show sales till oct 23

--- data/mailreport.py
+++ data/mailreport.py
@@ -3,6 +3,10 @@ from envelopes import Envelope, GMailSMTP
 import getpass
 import sqlite3
 import datetime
+import logging
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
 
 dryRun = False
 secrets = open(".mailreport-secret").read().strip().split(";")
@@ -136,20 +140,48 @@ def get_birthdayriders():
     return io.getvalue()
 
 
-def send_report(report, subj):
+def send_report(report, subj, recips): 
+	recips = recips or ['frontdesk@joyrideridgefield.com', 'info@joyrideridgefield.com'],
 	envelope = Envelope(
 	    from_addr=(u'joyride.robot@gmail.com', u'JoyRide Robot'),
-	    to_addr=['frontdesk@joyrideridgefield.com', 'info@joyrideridgefield.com'],
-            #to_addr=['kevin.manley@gmail.com'],
+	    to_addr=recips,
 	    subject=subj,
 	    html_body=report
 	)
 
+	log.info("sending email '%s' to '%s'" % (subj, repr(recips)))
 	# Send the envelope using an ad-hoc connection...
 	envelope.send('smtp.googlemail.com', login=secrets[0], password=secrets[1],
 					tls=True, port=587)
 
-def main():
+def fashionshow_sales():
+	# quick hack to keep an eye on fashion show
+	rows = list(conn.cursor().execute("select count(*)  from sale where item='VIP Ticket' and total='75.0';").fetchall())
+	vip = rows[0][0]
+	rows = list(conn.cursor().execute("select count(*)  from sale where item='General Admission' and total='50.0';").fetchall())
+	genl = rows[0][0]
+	total = genl + vip
+	
+	io = StringIO.StringIO()
+	io.write("<html><body>")
+	rows = list(conn.cursor().execute("select firstname, lastname, item, total, dt, pmtype from sale where (item='VIP Ticket' and total='75.0') or (item='General Admission' and total='50.0') order by lastname, firstname;").fetchall())
+	if rows:
+		io.write("<table border='1' cellpadding='1' cellspacing='1' bordercolor='#aaaaaa'>")
+		io.write("<tr><th>#</th><th>First name</th><th>Last Name</th><th>Ticket</th><th>Price</th><th>Purch Dt</th><th>Pmt Method</th></tr>")
+		for i, row in enumerate(rows):
+			firstname, lastname, item, price, dt, pmtype = row
+			dt = dt[:16]
+			io.write("<tr><td align='right'>%d</td><td>%s</td><td>%s</td><td>%s</td><td align='right'>%s</td><td>%s</td><td>%s</td></tr>" % \
+			    (i+1, firstname, lastname, item, price, dt, pmtype))
+		io.write("</table>")
+	else:
+		io.write("None")
+	io.write("</body></html>")
+	#open(r"/tmp/test2.html", "w").write(io.getvalue())
+	send_report(io.getvalue(), "Joy of Art sales: %d General, %d VIP, %d Total" % (genl, vip, total),
+	              ['kevin@joyrideridgefield.com', 'amypal@joyrideridgefield.com'])
+
+def rider_report():
 	limit = 20
 	io = StringIO.StringIO()
 	io.write("<html><body>")
@@ -183,4 +215,5 @@ def main():
 		send_report(io.getvalue(), 'Rider report for %s' % str(TODAY))
 
 if __name__ == "__main__":
-    main()
+    rider_report()
+    fashionshow_sales()
