e6cc686f9fea32f308381f129580d456f919fc06
Kevin Manley <kevin.manley+github@gmail.com>
2014-11-01 10:17:41 -0400

    free up some horizontal space

--- scrapeocc/mailreport.py
+++ scrapeocc/mailreport.py
@@ -18,7 +18,7 @@ def get_table(dt):
     rows = list(conn.cursor().execute("select * from v_occ where date(dt) = ? order by case when site='ridgefield' then 1 when site='westport' then 2 when site='darien' then 3 when site='texas' then 4 else site end, dt;", (dt,)).fetchall())
     #rows = curs.fetchall()
     io.write("<table border='1' cellpadding='1' cellspacing='1' bordercolor='#aaaaaa'>")
-    io.write("<tr><th>Date/time</th><th>Studio</th><th>Instr/Count</th><th>Sold</th><th>Total</th><th>Occupancy</th></tr>")
+    io.write("<tr><th>Date/time</th><th>Studio</th><th>Instr/Count</th><th>Sold</th><th>Total</th><th>Occ%</th></tr>")
     for i, row in enumerate(rows):
         row = list(row) # so we can edit it
         # strip off seconds
@@ -40,7 +40,7 @@ def get_history_by_studio():
     rows = list(conn.cursor().execute("select * from vw_occyymm where yymm>=strftime('%Y-%m',date('now','-2 months','start of month')) order by yymm, case when site='ridgefield' then 1 when site='westport' then 2 when site='darien' then 3 when site='texas' then 4 else site end;").fetchall())
     #rows = curs.fetchall()
     io.write("<table border='1' cellpadding='1' cellspacing='1' bordercolor='#aaaaaa'>")
-    io.write("<tr><th>Month</th><th>Studio</th><th># Classes</th><th>Sold</th><th>Total</th><th>Riders/Class</th><th>Occupancy</th></tr>")
+    io.write("<tr><th>Month</th><th>Studio</th><th>Classes</th><th>Sold</th><th>Total</th><th>Avg/Cls</th><th>Occ%</th></tr>")
     for i, row in enumerate(rows):
         io.write("<tr><td>%s</td><td>%s</td><td align='right'>%d</td><td align='right'>%d</td><td align='right'>%d</td><td align='right'>%.1f</td><td align='right'>%.1f%%</td></tr>" % row)
     io.write("</table>")
@@ -52,7 +52,7 @@ def get_history_by_instr():
     rows = list(conn.cursor().execute("select * from vw_occyymmbyinstr where yymm>=strftime('%Y-%m',date('now','-1 months','start of month')) order by yymm, case when site='ridgefield' then 1 when site='westport' then 2 when site='darien' then 3 when site='texas' then 4 else site end, instr;").fetchall())
     #rows = curs.fetchall()
     io.write("<table border='1' cellpadding='1' cellspacing='1' bordercolor='#aaaaaa'>")
-    io.write("<tr><th>Month</th><th>Studio</th><th>Instr</th><th># Classes</th><th>Sold</th><th>Total</th><th>Riders/Class</th><th>Occupancy</th></tr>")
+    io.write("<tr><th>Month</th><th>Studio</th><th>Instr</th><th>Classes</th><th>Sold</th><th>Total</th><th>Avg/Cls</th><th>Occ%</th></tr>")
     for i, row in enumerate(rows):
         io.write("<tr><td>%s</td><td>%s</td><td>%s</td><td align='right'>%d</td><td align='right'>%d</td><td align='right'>%d</td><td align='right'>%.1f</td><td align='right'>%.1f%%</td></tr>" % row)
     io.write("</table>")
